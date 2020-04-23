ea329486b42180da517d3cc09c342f9efb857b17
Vadim Chistokhvalov <ivadik05@gmail.com>
2020-04-23 22:03:17 +0000

    Deploy vadim-ch/workflow-board to github.com/vadim-ch/workflow-board.git:gh-pages

---
+++ precache-manifest.85655e2bb6d2d189a29fb05004f439d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b38af7c16716bfe8c9092a9494b575",
+    "url": "/workflow-board/index.html"
+  },
+  {
+    "revision": "470cb5e2ed25d975ae05",
+    "url": "/workflow-board/static/css/main.8343939b.chunk.css"
+  },
+  {
+    "revision": "05a2c97b43121bfeb1b0",
+    "url": "/workflow-board/static/js/2.4d0c7390.chunk.js"
+  },
+  {
+    "revision": "470cb5e2ed25d975ae05",
+    "url": "/workflow-board/static/js/main.2ebf03ab.chunk.js"
+  },
+  {
+    "revision": "231ae54b5034948d3605",
+    "url": "/workflow-board/static/js/runtime~main.cda9c3ef.js"
+  }
+]);
